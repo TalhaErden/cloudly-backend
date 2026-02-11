f03c0dd3ebf1c3509afebc2d50aa308699d33f3f
talhaerden <talhareys@gmail.com>
2026-02-11 10:32:47 +0300

    complete backend logic with move task challenge and validation

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -1,10 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateOrganizationDto {
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @IsOptional()
   @IsString()
   address?: string; // Soru işareti opsiyonel demek
 }
